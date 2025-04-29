26553a02d48839e4ee392a282796920e92ada8c3
Bikash Shaw <vshaw138@gmail.com>
2025-04-29 18:53:09 +0530

    Added  Zod

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,1 +1,27 @@
 import { client } from "@repo/db/client";
+import { SignInSchema, SignUpSchema, CreateRoomSchema } from "@repo/common/zodTypes"
+import express from 'express'
+import cors from "cors"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.post("signup", (req,res)=>{
+    try{
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: error
+        })
+    }
+})
+
+const PORT = process.env.PORT || 4000
+app.listen( PORT =>{
+    console.log(`Server is up at PORT ${PORT}`)
+})
+
+
---
+++ packages/common/src/index.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const SignUpSchema = z.object({
+    email: z.string().min(4).max(40),
+    password: z.string(),
+    name: z.string()
+})
+export const SignInSchema = z.object({
+    email: z.string().min(4).max(40),
+    password: z.string(),
+})
+export const CreateRoomSchema = z.object({
+    name: z.string()
+})
