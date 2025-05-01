94dcb4f4ce5e64ac9880207b997c02a231d3824e
Bikash Shaw <vshaw138@gmail.com>
2025-05-01 12:58:23 +0530

    Updated Ws Server

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,24 +1,48 @@
 'use client'
 import { useEffect, useMemo, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 const URL = "ws://localhost:8080"
 
 
 export default function Home() {
   
+  const {
+    register,
+    handleSubmit,
+    reset
+  } = useForm()
   //@ts-ignore
   const wsRef = useRef()
-  const [message, setMessage] = useState('')
+  const [messages, setMessage] = useState([""])
 
-
-  const sendMessage= () => {
+  //@ts-ignore
+  const sendMessage= (data) => {
     //@ts-ignore
-    wsRef.current.send(message)
-    setMessage('')
+    wsRef.current.send(JSON.stringify({
+      type: "chat",
+      payload: data
+    }))
+    reset({message: ""})
   }
+
   
   useEffect(()=>{
     const ws = new WebSocket(URL)
+    ws.onmessage = (a) => {
+      setMessage(m => [...m, a.data])
+    }
     wsRef.current = ws
+    //@ts-ignore
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join-room",
+        payload: {
+          roomId: "1"
+      }
+    }))}
+    return () => {
+      ws.close()
+    }
   },[])
   
   //@ts-ignore
@@ -28,13 +52,18 @@ export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
         <div className="flex flex-col">
-          
-          <input value={message}  onChange={e => setMessage(e.target.value)} placeholder="Message"/>
-          <button onClick={sendMessage}
-            className="cursor-pointer bg-orange-500 p-2 rounded-2xl mt-2 text-center">
-            Send Message
-          </button>
-          
+          {
+            messages.map((message)=>(
+              <p>{message}</p>
+            ))
+          }
+          <form onSubmit={handleSubmit(sendMessage)}>
+            <input {...register("message")} name="message" placeholder="Message"/>
+            <button
+              className="cursor-pointer bg-orange-500 p-2 rounded-2xl mt-2 text-center">
+              Send Message
+            </button>
+          </form>
         </div>
     </div>
   );
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -4,10 +4,33 @@ const PORT = 8080
 const ws = new WebSocketServer({port: PORT})
 console.log(`WS Server is running at PORT: ${PORT}`)
 
+//@ts-ignore
+const allSockets = []
+
 ws.on('connection', (socket)=>{
     console.log("User Connected")
 
     socket.on("message", (message)=>{
-        console.log(message.toString())
+        const parsedMessage = JSON.parse(message as unknown as string)
+        if(parsedMessage.type == 'join-room'){
+            const roomId = parsedMessage.payload.roomId
+            console.log(parsedMessage)
+            allSockets.push({
+                socket,
+                room: roomId
+            })
+        }
+        if(parsedMessage.type == 'chat'){
+            console.log("User Want to Chat")
+            console.log(parsedMessage)
+            //@ts-ignore
+            const currentUserRoomId = allSockets.find((user) => user.socket === socket)?.room
+            //@ts-ignore
+            allSockets
+            //@ts-ignore
+                .filter((user) => user.room === currentUserRoomId)
+                //@ts-ignore
+                .forEach((user) => user.socket.send(parsedMessage.payload.message))
+        }
     })
-})
+}) 
